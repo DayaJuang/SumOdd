15e3eb24e97f4aeee41863289c95461bf649f3d6
daya <dayajuangmahaputra@gmail.com>
2022-11-09 14:09:05 +0900

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,32 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ans = sumOdd(200,-100);
+
+        System.out.println(ans);
+    }
+
+    public static boolean isOdd(int num){
+        // Check if the number is valid or not
+        if(num <= 0)
+            return false;
+
+        return (num % 2 != 0);
+    }
+
+    public static int sumOdd(int start, int end){
+        // Make sure end is greater than start
+        if(start > end || start < 0)
+            return -1;
+
+        // Summing the number
+        int sum = 0;
+
+        for(int i=start;i<=end;i++){
+            if(isOdd(i))
+                sum+=i;
+        }
+
+        return sum;
     }
 }
